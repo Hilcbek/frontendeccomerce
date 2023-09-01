e64ac57bc3b3d295874664a70d52035d90383f1e
Hilcbek <balemayehu07@gmail.com>
2023-09-01 12:14:30 +0300

    added frontend 2

--- src/components/CatagoryItem.jsx
+++ src/components/CatagoryItem.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import {HiExternalLink} from 'react-icons/hi'
+import { useSelector } from 'react-redux'
 import {Link} from 'react-router-dom' 
 const CatagoryItem = ({data,data2}) => {
+  let {username} = useSelector((state) => state.user)
   let handleTop = () => {
     window.scrollTo({
       top : 0,
@@ -15,7 +17,7 @@ const CatagoryItem = ({data,data2}) => {
         </div>
         {
           data?.map((cat) => (
-              <Link onClick={() => handleTop()} to={`/product/RelatedProducts/${cat}`} className='md:absolute transition_cubic text-3xl font-light tracking-widest flex items-center justify-center p-3 bg-black/80 w-11/12 rounded-md cursor-pointer text-white my-3 button scale-100 z-[999]'>{cat} <HiExternalLink className={'ml-2'} /></Link>
+              <Link onClick={() => handleTop()} to={username ? `/product/RelatedProducts/${cat}` : '/login'} className='md:absolute transition_cubic text-3xl font-light tracking-widest flex items-center justify-center p-3 bg-black/80 w-11/12 rounded-md cursor-pointer text-white my-3 button scale-100 z-[999]'>{cat} <HiExternalLink className={'ml-2'} /></Link>
           ))
         }
     </div>
